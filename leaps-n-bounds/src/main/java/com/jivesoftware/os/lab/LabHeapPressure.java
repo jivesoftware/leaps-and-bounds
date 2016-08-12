4e6654b4f65255ce994f23b8076ed7d745044794
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2016-08-12 12:54:37 -0500

    guard against overly frequent heap pressure commit checks

--- leaps-n-bounds/src/main/java/com/jivesoftware/os/lab/LabHeapPressure.java
+++ leaps-n-bounds/src/main/java/com/jivesoftware/os/lab/LabHeapPressure.java
@@ -8,6 +8,7 @@ import com.jivesoftware.os.mlogger.core.ValueType;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -21,6 +22,7 @@ public class LabHeapPressure {
     private final long maxHeapPressureInBytes;
     private final AtomicLong globalHeapCostInBytes;
     private final Map<LAB, Boolean> labs = new ConcurrentHashMap<>();
+    private final AtomicBoolean running = new AtomicBoolean();
 
     public LabHeapPressure(long maxHeapPressureInBytes, AtomicLong globalHeapCostInBytes) {
         this.maxHeapPressureInBytes = maxHeapPressureInBytes;
@@ -44,32 +46,40 @@ public class LabHeapPressure {
         long globalHeap = globalHeapCostInBytes.get();
         LOG.set(ValueType.VALUE, "lab>heap>pressure", globalHeap);
         LOG.set(ValueType.VALUE, "lab>commitable", labs.size());
-        if (globalHeap > maxHeapPressureInBytes) {
+        if (globalHeap > maxHeapPressureInBytes && running.compareAndSet(false, true)) {
             lab.schedule.submit(() -> {
-                LAB[] keys = labs.keySet().toArray(new LAB[0]);
-                Arrays.sort(keys, (LAB o1, LAB o2) -> {
-                    return -Long.compare(o1.approximateHeapPressureInBytes(), o2.approximateHeapPressureInBytes());
-                });
-                int i = 0;
-                while (i < keys.length && globalHeapCostInBytes.get() > maxHeapPressureInBytes) {
-                    Boolean efsyncOnFlush = labs.remove(keys[i]);
-                    if (efsyncOnFlush != null) {
-                        try {
-                            LOG.inc("lab>commit>global>" + efsyncOnFlush);
-                            LOG.set(ValueType.VALUE, "lab>commitable", labs.size());
-                            LOG.debug("Forcing flush due to heap pressure. lab:{}", lab);
-                            keys[i].commit(efsyncOnFlush, false); // todo config
-                        } catch (LABIndexCorruptedException | LABIndexClosedException x) {
-                        } catch (Exception x) {
-                            labs.compute(keys[i], (LAB t, Boolean u) -> {
-                                return u == null ? efsyncOnFlush : (boolean) u || efsyncOnFlush;
-                            });
-                            throw x;
-                        }
+                try {
+                    if (globalHeapCostInBytes.get() < maxHeapPressureInBytes) {
+                        LOG.inc("lab>commit>global>skip");
+                        return null;
                     }
-                    i++;
+                    LAB[] keys = labs.keySet().toArray(new LAB[0]);
+                    Arrays.sort(keys, (LAB o1, LAB o2) -> {
+                        return -Long.compare(o1.approximateHeapPressureInBytes(), o2.approximateHeapPressureInBytes());
+                    });
+                    int i = 0;
+                    while (i < keys.length && globalHeapCostInBytes.get() > maxHeapPressureInBytes) {
+                        Boolean efsyncOnFlush = labs.remove(keys[i]);
+                        if (efsyncOnFlush != null) {
+                            try {
+                                LOG.inc("lab>commit>global>" + efsyncOnFlush);
+                                LOG.set(ValueType.VALUE, "lab>commitable", labs.size());
+                                LOG.debug("Forcing flush due to heap pressure. lab:{}", lab);
+                                keys[i].commit(efsyncOnFlush, false); // todo config
+                            } catch (LABIndexCorruptedException | LABIndexClosedException x) {
+                            } catch (Exception x) {
+                                labs.compute(keys[i], (LAB t, Boolean u) -> {
+                                    return u == null ? efsyncOnFlush : (boolean) u || efsyncOnFlush;
+                                });
+                                throw x;
+                            }
+                        }
+                        i++;
+                    }
+                    return null;
+                } finally {
+                    running.set(false);
                 }
-                return null;
             });
         }
     }
