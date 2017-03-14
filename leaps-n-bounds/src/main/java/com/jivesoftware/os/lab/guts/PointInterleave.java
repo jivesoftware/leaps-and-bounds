a76f5daaf5034652f3cc6c3321d6374d96693f20
Jonathan Colt <jonathan.colt@jivesoftware.com>
2017-03-14 10:46:02 -1000

    ensure point interleave returns null on miss

--- leaps-n-bounds/src/main/java/com/jivesoftware/os/lab/guts/PointInterleave.java
+++ leaps-n-bounds/src/main/java/com/jivesoftware/os/lab/guts/PointInterleave.java
@@ -19,18 +19,18 @@ public class PointInterleave implements Scanner, RawEntryStream {
     private final Rawhide rawhide;
     private FormatTransformer nextReadKeyFormatTransformer;
     private FormatTransformer nextReadValueFormatTransformer;
+    private boolean once;
     private BolBuffer nextRawEntry;
-
-    private final BolBuffer leftKeyBuffer = new BolBuffer();
-    private final BolBuffer rightKeyBuffer = new BolBuffer();
 
 
     public PointInterleave(ReadIndex[] indexs, byte[] key, Rawhide rawhide, boolean hashIndexEnabled) throws Exception {
         this.rawhide = rawhide;
+        BolBuffer entryBuffer = new BolBuffer();
+        BolBuffer entryKeyBuffer = new BolBuffer();
         for (int i = 0; i < indexs.length; i++) {
             Scanner scanner = null;
             try {
-                scanner = indexs[i].pointScan(new ActiveScan(hashIndexEnabled), key, new BolBuffer(), new BolBuffer());
+                scanner = indexs[i].pointScan(new ActiveScan(hashIndexEnabled), key, entryBuffer, entryKeyBuffer);
                 if (scanner != null) {
                     scanner.next(this);
                     scanner.close();
@@ -46,11 +46,11 @@ public class PointInterleave implements Scanner, RawEntryStream {
 
     @Override
     public Next next(RawEntryStream stream) throws Exception {
-        if (nextRawEntry == null) {
+        if (once) {
             return Next.stopped;
         }
         stream.stream(nextReadKeyFormatTransformer, nextReadValueFormatTransformer, nextRawEntry);
-        nextRawEntry = null;
+        once = true;
         return Next.more;
     }
 
